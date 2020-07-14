4bef4f7df67175846074b2c49bbc53b1ce8d4964
Piotr Nowak <pinotestpiotrnowak@gmail.com>
2020-07-14 20:09:00 +0200

    Add dynamic menu

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import tmdb_client
-from flask import Flask, render_template
+import random
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+LIST_TYPE = ['top_rated', 'upcoming', 'now_playing', 'popular']
 
 
 @app.route('/')
 def homepage():
-    movies = tmdb_client.get_movies(how_many=8)
-    return render_template("homepage.html", movies=movies)
+    selected_list = request.args.get('list_type', "popular")
+    movies = tmdb_client.get_movies(how_many=8, list_type=selected_list)
+    return render_template("homepage.html", movies=movies, current_list=selected_list, LIST_TYPE=LIST_TYPE)
 
 
 @app.context_processor
@@ -21,7 +25,9 @@ def utility_processor():
 def movie_details(movie_id):
     details = tmdb_client.get_single_movie(movie_id)
     cast = tmdb_client.get_single_movie_cast(movie_id)
-    return render_template("movie_details.html", movie=details, cast=cast)
+    movie_images = tmdb_client.get_movie_images(movie_id)
+    selected_backdrop = random.choice(movie_images['backdrops'])
+    return render_template("movie_details.html", movie=details, cast=cast, selected_backdrop=selected_backdrop)
 
 
 if __name__ == '__main__':
--- tmdb_client.py
+++ tmdb_client.py
@@ -17,8 +17,8 @@ def get_poster_url(poster_api_path, size="w342"):
     return f"{base_url}{size}/{poster_api_path}"
 
 
-def get_movies(how_many):
-    data = get_popular_movies()
+def get_movies(how_many, list_type='popular'):
+    data = get_movies_list(list_type)
     return random.sample(data['results'], how_many)
 
 
@@ -38,3 +38,22 @@ def get_single_movie_cast(movie_id):
     }
     response = requests.get(endpoint, headers=headers)
     return response.json()["cast"]
+
+
+def get_movie_images(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/images"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+
+def get_movies_list(list_type):
+    endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    response.raise_for_status()
+    return response.json()
